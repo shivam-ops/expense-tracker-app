7510d36c570a6f4a2e94a6d48d32dfb43add69af
Shivam Joshi <shivamjoshi0822@gmail.com>
2021-01-18 21:21:24 +0530

    fetching categories from spring boot

---
+++ src/Category.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Category extends Component {
+  state = {
+    isloading: true,
+    Categories: [],
+  };
+
+  async componentDidMount() {
+    const response = await fetch("/api/categories");
+    const body = await response.json();
+    this.setState({ Categories: body, isloading: false });
+  }
+  render() {
+    const { Categories, isloading } = this.state;
+    if (isloading) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h2>Categories</h2>{" "}
+        {Categories.map((category) => (
+          <div id={category.id}>{category.name}</div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Category;
