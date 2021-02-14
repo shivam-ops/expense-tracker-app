cc6b8010f146c5fc7cbfa0f68b618af8ea759418
Shivam Joshi <shivamjoshi0822@gmail.com>
2021-02-14 22:16:00 +0530

    adding form for expenses

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,13 +1,69 @@
 import React, { Component } from "react";
 import AppNav from "./AppNav";
+import DatePicker from "react-datepicker";
+import "./App.css";
+import "react-datepicker/dist/react-datepicker.css";
+import { Container, Input, Form, Button, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
 
 class Expenses extends Component {
   state = {};
   render() {
+    const title = <h3>Add Expense</h3>;
     return (
       <div>
         <AppNav />
-        <h2>Expenses</h2>
+        <Container>
+          {title}
+          <Form>
+            <FormGroup>
+              <label for="title">Title</label>
+              <Input
+                type="text"
+                name="title"
+                id="title"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label for="category">Category</label>
+              <Input
+                type="text"
+                name="category"
+                id="category"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label for="expenseDate">Expense Date</label>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label for="location">Location</label>
+              <Input
+                type="text"
+                name="location"
+                id="location"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Button color="primary" type="submit">
+                Save
+              </Button>{" "}
+              <Button color="secondary" tag={Link} to="/">
+                Cancel
+              </Button>
+            </FormGroup>
+          </Form>
+        </Container>
       </div>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,21 @@ import AppNav from "./AppNav";
 class Home extends Component {
   state = {};
   render() {
-    return <AppNav />;
+    return (
+      <div>
+        <AppNav />
+        <h2
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignitems: "center",
+            height: "100vh",
+          }}
+        >
+          Welcome to Easy Expese App!
+        </h2>
+      </div>
+    );
   }
 }
 
