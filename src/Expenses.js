49ffe65d2c4913abdc0204f151ab312e7cd3adb4
Shivam Joshi <shivamjoshi0822@gmail.com>
2021-08-04 23:55:17 +0530

    some correction for date picker

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,125 +1,140 @@
-import React, { Component } from "react";
-import AppNav from "./AppNav";
-import DatePicker from "react-datepicker";
-import "./App.css";
+import React, { Component } from 'react';
+import AppNav from './AppNav';
+import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-import { Table, Container, Input, Form, Button, FormGroup } from "reactstrap";
-import { Link } from "react-router-dom";
+import './App.css';
+import { Table,Container,Input,Button,Label, FormGroup, Form} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import Moment from 'react-moment';
 
 class Expenses extends Component {
-  emptyItem = {
-    id: "103",
-    expenseDate: new Date(),
-    description: "",
-    location: "",
-    categories: [1, "Travel"],
-  };
 
-  constructor(props) {
-    super(props);
+  // {
+  //   "id": 100,
+  //   "expensedate": "2019-06-16T17:00:00Z",
+  //   "description": "New York Business Trip",
+  //   "location": "New York",
+  //   "category": {
+  //   "id": 1,
+  //   "name": "Travel"
+  //   }
+  //   },
+ 
+    emptyItem = {
+        description : '' ,
+        expenseDate : new Date(),
+        id:104,
+        location : '',
+        category : {id:1 , name:'Travel'}
+    }
+    
+    constructor(props){
+      super(props)
 
-    this.state = {
-      date: new Date(),
-      isLoading: true,
-      expenses: [],
-      categories: [],
-      item: this.emptyItem,
-    };
+      this.state = { 
+        isLoading :false,
+        categories:[],
+        expenses : [],
+        date :new Date(),
+        item : this.emptyItem
+       }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDateChange = this.handleDateChange(this);
-  }
+       this.handleSubmit= this.handleSubmit.bind(this);
+       this.handleChange= this.handleChange.bind(this);
+       this.handleDateChange= this.handleDateChange.bind(this);
 
-  async handleSubmit(event) {
-    event.preventDefault();
-    const { item } = this.state;
-    await fetch(`/api/expenses`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(item),
-    });
+    } 
 
-    console.log(this.state);
-    this.props.history.push("/expenses");
-  }
-
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    let item = { ...this.state.item };
-    item[name] = value;
-    this.setState({ item });
-    console.log(this.state.item);
-  }
-
-  handleDateChange(date) {
-    let item = { ...this.state.item };
-    item.expenseDate = date;
-
-    this.setState({ item });
-  }
-
-  async remove(id) {
-    await fetch(`api/expenses/${id}`, {
-      method: "DELETE",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    }).then(() => {
-      let updatedExpenses = [...this.state.expenses].filter((i) => i.id !== id);
-      this.setState({ expenses: updatedExpenses });
-    });
-  }
-
-  async componentDidMount() {
-    const response = await fetch("/api/categories");
-    const body = await response.json();
-
-    this.setState({ categories: body, isLoading: false });
-
-    const responseExp = await fetch("/api/expenses");
-    const bodyExp = await responseExp.json();
-
-    this.setState({ expenses: bodyExp, isLoading: false });
-  }
-
-  render() {
-    const title = <h3>Add Expense</h3>;
-    const { categories } = this.state;
-    const { expenses, isLoading } = this.state;
-
-    if (isLoading) {
-      return <div>Loading...</div>;
+    async handleSubmit(event){
+     
+      const item = this.state.item;
+      await fetch(`/api/expenses`, {
+        method : 'POST',
+        headers : {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body : JSON.stringify(item),
+      });
+      
+      event.preventDefault();
+      this.props.history.push("/expenses");
     }
 
-    let optionList = categories.map((category) => (
-      <option id={category.id}>{category.name}</option>
-    ));
+    handleChange(event){
+      const target= event.target;
+      const value= target.value;
+      const name = target.name;
+      let item={...this.state.item};
+      item[name] = value;
+      this.setState({item});
+      console.log(item);
+    }
 
-    let rows = expenses.map((expense) => (
-      <tr key={expense.id}>
-        <td>{expense.description}</td>
-        <td>{expense.location}</td>
-        <td>{expense.expensedate}</td>
-        <td>{expense.category.name}</td>
-        <td>
-          <Button
-            size="sm"
-            color="danger"
-            onClick={() => this.remove(expense.id)}
-          >
-            Delete
-          </Button>
-        </td>
-      </tr>
-    ));
+
+    handleDateChange(date){
+      let item={...this.state.item};
+      item.expenseDate= date;
+      this.setState({item});
+    
+    }
+
+
+    async remove(id){
+        await fetch(`/api/expenses/${id}` , {
+          method: 'DELETE' ,
+          headers : {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+          }
+
+        }).then(() => {
+          let updatedExpenses = [...this.state.Expsenses].filter(i => i.id !== id);
+          this.setState({Expsenses : updatedExpenses});
+        });
+
+    }
+
+    async componentDidMount() {
+        const response= await fetch('/api/categories');
+        const body= await response.json();
+        this.setState({Categories : body , isLoading :false});
+
+
+        // const responseExp= await fetch('/api/expenses');
+        // const bodyExp = await responseExp.json();
+        // this.setState({Expenses : bodyExp , isLoading :false});
+        // console.log(bodyExp);
+
+    }
+
+    render() { 
+        const title =<h3>Add Expense</h3>;
+        const {expenses, categories, isLoading} =this.state;
+  
+        if (isLoading)
+            return(<div>Loading....</div>)
+
+        let optionList  =
+                categories.map( (category) =>
+                    <option value={category.id} key={category.id}>
+                                {category.name} 
+                    </option>
+                )
+        
+        let rows=
+            expenses.map( expense =>
+              <tr key={expense.id}>
+                <td>{expense.description}</td>
+                <td>{expense.location}</td>
+                <td><Moment date={expense.expensedate} format="YYYY/MM/DD"/></td>
+                <td>{expense.category.name}</td>
+                <td><Button size="sm" color="danger" onClick={() => this.remove(expense.id)}>Delete</Button></td>
+
+              </tr>
+
+
+            )
 
     return (
       <div>
@@ -152,8 +167,8 @@ class Expenses extends Component {
             <FormGroup>
               <label for="city">Date</label>
               <DatePicker
-                selected={this.state.date}
-                onChange={this.handleChange}
+                selected={this.state.item.expenseDate}
+                onChange={this.handleDateChange}
               />
             </FormGroup>
 
